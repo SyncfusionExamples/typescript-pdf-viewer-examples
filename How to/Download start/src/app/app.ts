b62d529fe6d66bedf4fb6cd48ed31d297570ffaa
SaravanaPriya31 <Saravana31>
2024-06-10 18:51:11 +0530

    889569: commit

---
+++ How to/Download start/src/app/app.ts
@@ -0,0 +1,12 @@
+import { PdfViewer, Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner} from '@syncfusion/ej2-pdfviewer';
+
+PdfViewer.Inject(Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner);
+
+let pdfviewer: PdfViewer = new PdfViewer();
+pdfviewer.documentPath = "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf";
+pdfviewer.resourceUrl = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
+pdfviewer.appendTo('#PdfViewer');
+pdfviewer.downloadStart = (args: any) => {
+    // Your custom logic here
+    args.cancel = true; // Prevent download action
+};
---
+++ How to/selectSignature and unSelectSignature property/src/app/app.ts
@@ -0,0 +1,15 @@
+import { PdfViewer, Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner} from '@syncfusion/ej2-pdfviewer';
+
+PdfViewer.Inject(Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner);
+
+let pdfviewer: PdfViewer = new PdfViewer();
+pdfviewer.documentPath = "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf";
+pdfviewer.resourceUrl = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
+pdfviewer.appendTo('#PdfViewer');
+pdfviewer.signatureSelect = (args: any) => {
+    console.log('Signature selected:', args);
+ };
+ 
+pdfviewer.pageRenderComplete = (args: any) => {
+    console.log('Signature selected:', args);
+};
