77366b923a2c411569e49623ec46d946a826941d
Vincentsf4657 <vincentkumar.l@syncfusion.com>
2024-09-19 18:46:17 +0530

    906190: UGDocumentation

--- How to/Customization  of Print Quality/src/app/app.ts
+++ How to/Customization  of Print Quality/src/app/app.ts
@@ -5,5 +5,6 @@ PdfViewer.Inject(Toolbar, Magnification, Navigation, Annotation, LinkAnnotation,
 let pdfviewer: PdfViewer = new PdfViewer();
 pdfviewer.documentPath = "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf";
 pdfviewer.resourceUrl = "https://cdn.syncfusion.com/ej2/27.1.48/dist/ej2-pdfviewer-lib";
+//By changing value of the printScaleFactor we can adjust the quality of the  PDF file.
 pdfviewer.printScaleFactor=0.5;
 pdfviewer.appendTo('#PdfViewer');
