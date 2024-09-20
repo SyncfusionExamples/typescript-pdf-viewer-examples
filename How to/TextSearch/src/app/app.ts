494ad423c7085d18ad580ad12fc27e964039a1ed
GiriSF4292 <giridharmalayakonda.m@syncfusion.com>
2024-09-20 12:22:15 +0530

    897679: committed the changes regarding text search sample

---
+++ How to/TextSearch/src/app/app.ts
@@ -0,0 +1,22 @@
+import { PdfViewer, Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner} from '@syncfusion/ej2-pdfviewer';
+
+PdfViewer.Inject(Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner);
+
+let pdfviewer: PdfViewer = new PdfViewer();
+pdfviewer.documentPath = "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf";
+pdfviewer.resourceUrl = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
+
+pdfviewer.appendTo('#PdfViewer');
+let textbounds = document.getElementById('textbounds');
+if(textbounds) {
+    textbounds.addEventListener('click', function() {
+        // Find and get the bounds of a text
+        console.log(pdfviewer.textSearch.findText('adobe', false));
+        // Find and get the bounds of a text on the desired page
+        console.log(pdfviewer.textSearch.findText('adobe', false, 7));
+        // Find and get the bounds of the list of text
+        console.log(pdfviewer.textSearch.findText(['adobe', 'pdf'], false));
+        // Find and get the bounds of the list of text on desired page
+        console.log(pdfviewer.textSearch.findText(['adobe', 'pdf'], false, 7));
+    });
+}
