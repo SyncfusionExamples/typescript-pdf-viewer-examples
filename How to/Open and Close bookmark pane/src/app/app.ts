b5afd32b51024863b7dbece579a95bee5e84feda
Vincentsf4657 <vincentkumar.l@syncfusion.com>
2024-09-27 13:08:14 +0530

    906190: UGDocumentation

--- How to/Open and Close bookmark pane/src/app/app.ts
+++ How to/Open and Close bookmark pane/src/app/app.ts
@@ -1,18 +1,9 @@
-import { PdfViewer, Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner} from '@syncfusion/ej2-pdfviewer';
+import { PdfViewer, Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner, Print} from '@syncfusion/ej2-pdfviewer';
 
-PdfViewer.Inject(Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner);
+PdfViewer.Inject(Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, ThumbnailView, BookmarkView, TextSelection, TextSearch, FormFields, FormDesigner, Print);
 
 let pdfviewer: PdfViewer = new PdfViewer();
 pdfviewer.documentPath = "https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf";
-pdfviewer.resourceUrl = "https://cdn.syncfusion.com/ej2/23.1.43/dist/ej2-pdfviewer-lib";
-
+pdfviewer.resourceUrl = "https://cdn.syncfusion.com/ej2/27.1.48/dist/ej2-pdfviewer-lib";
+pdfviewer.printScaleFactor=0.5;
 pdfviewer.appendTo('#PdfViewer');
-document.getElementById('openBookmark').addEventListener('click', () => {
-    // Open Bookmark pane
-    pdfviewer.bookmarkViewModule.openBookmarkPane();
-});
-
-document.getElementById('closeBookmark').addEventListener('click', () => {
-    // close Bookmark pane
-    pdfviewer.bookmarkViewModule.closeBookmarkPane();
-});
