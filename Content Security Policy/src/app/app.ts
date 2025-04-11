d8de5b6dc383a270a0acbf6b05ffaf65d2d3e514
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-04-11 18:05:02 +0530

    951648: Updated code

--- Content Security Policy/src/app/app.ts
+++ Content Security Policy/src/app/app.ts
@@ -4,13 +4,7 @@ import { PdfViewer, TextSelection, TextSearch, Print, Navigation, Toolbar, Magni
 // Inject required modules
 PdfViewer.Inject(TextSelection, TextSearch, Print, Navigation, Toolbar, Magnification, Annotation, FormDesigner, FormFields);
 
-// const pdfviewer: PdfViewer = new PdfViewer({
-//     documentPath: 'https://cdn.syncfusion.com/content/pdf/pdf-succinctly.pdf',
-//     resourceUrl: "https://cdn.syncfusion.com/ej2/27.2.5/dist/ej2-pdfviewer-lib",
-// });
-// pdfviewer.appendTo('#PdfViewer');
-
 const pdfviewer: PdfViewer = new PdfViewer();
 pdfviewer.documentPath = "https://cdn.syncfusion.com/content/pdf/gis-succinctly.pdf";
-pdfviewer.resourceUrl="https://cdn.syncfusion.com/ej2/29.1.35/dist/ej2-pdfviewer-lib";
+pdfviewer.resourceUrl="https://cdn.syncfusion.com/ej2/29.1.37/dist/ej2-pdfviewer-lib";
 pdfviewer.appendTo('#PdfViewer');
